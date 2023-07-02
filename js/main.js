35174e3fa373ddc27291cae0fa3317ab57afe0a6
Andrew Chulkov <andrey.chulkov@list.ru>
2023-07-02 20:54:02 +0300

    tabs.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 import modals from './modals.js';
+import tabs from './tabs.js';
 
 window.addEventListener("DOMContentLoaded", () => {
   modals();
+  tabs('.glazing_slider ', '.glazing_block', '.glazing_content', 'active');
+  tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
 });
 
 
